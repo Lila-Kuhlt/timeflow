cc4fea082990bb666e11c2b033e8fb418986cd6c
nat-rix <upezu@student.kit.edu>
2025-04-02 23:42:45 +0200

    Geile radio buttons

---
+++ ui/components/tile_selector_tile.gd
@@ -0,0 +1,16 @@
+extends Panel
+class_name TileSelectorTile
+
+@onready var btn: TextureButton = $TextureButton
+var idx
+
+signal on_clicked(tile: TileSelectorTile)
+
+func on_click_handler():
+	on_clicked.emit(self)
+
+func on_select():
+	modulate = Color(0.5, 0.5, 0.5, 1.0)
+
+func on_deselect():
+	modulate = Color(1.0, 1.0, 1.0, 1.0)
---
+++ ui/screens/tile-selector/tile_selector_menu.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+signal tile_select_changed(idx: int)
+
+var tile_count := 3
+var tiles: Array[TileSelectorTile] = []
+var selected_tile := -1
+
+func _ready():
+	var tile_container = $MarginContainer/Panel/HBoxContainer
+	for i in range(tile_count):
+		var tilename := "TileSelectorTile%d" % (i+1)
+		var tile: TileSelectorTile = tile_container.get_node(tilename)
+		tile.idx = i
+		tile.connect("on_clicked", on_select_tile)
+		tiles.append(tile)
+	on_select_tile(tiles[0])
+
+func on_select_tile(tile: TileSelectorTile):
+	if selected_tile == tile.idx:
+		return
+	tiles[selected_tile].on_deselect()
+	tile.on_select()
+	selected_tile = tile.idx
+	tile_select_changed.emit(selected_tile)
