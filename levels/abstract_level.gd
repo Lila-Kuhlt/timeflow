4a68cf93360d3adfc7e0f3eefe77cc95acfc7f21
konsumlamm <konsumlamm@gmail.com>
2025-04-03 00:16:17 +0200

    Share enums

--- levels/abstract_level.gd
+++ levels/abstract_level.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const Shared = preload("shared.gd")
+
 @onready var tile_map : TileMapLayer = $PlaceableTileMap
 @onready var ghost_map : TileMapLayer = $GhostTileMap
 @onready var tile_selector : TileSelectorMenu = $TileSelectorMenu
@@ -18,31 +20,16 @@ func get_selected_tile() -> Vector2i:
 	var mouse_pos = get_viewport().get_mouse_position()
 	var map_coord = tile_map.local_to_map(to_local(mouse_pos))
 	return map_coord
-	
-func _process(delta: float):
+
+func _process(_delta: float):
 	var hovered_tile = get_selected_tile()
 	if hovered_tile != hovered_tile_before:
 		if hovered_tile_before != null:
 			ghost_map.set_cell(hovered_tile_before, -1, Vector2i(-1, -1), -1)  # removes cell
 		ghost_map.set_cell(hovered_tile, tile_selector.selected_tile, chosen_atlas_coord)
 		hovered_tile_before = hovered_tile
-		place_tile_on_coordinate(Vector2i(0, 0), Tile.STRAIGHT, Rotation.UP)
+		place_tile_on_coordinate(Vector2i(0, 0), Shared.Tile.STRAIGHT, Shared.Rotation.UP)
 
-func place_tile_on_coordinate(coords: Vector2i, type: Tile, rotation: Rotation):
-	
+func place_tile_on_coordinate(coords: Vector2i, type: Shared.Tile, rotation: Shared.Rotation):
 	var test := Vector2i(1, 1)
 	tile_map.set_cell(coords, 0, test)
-	
-enum Tile {
-	STRAIGHT,
-	CROSS,
-	CURVE,
-	T
-}
-
-enum Rotation {
-	UP,
-	LEFT,
-	DOWN,
-	RIGHT
-}
---
+++ levels/shared.gd
@@ -0,0 +1,13 @@
+enum Tile {
+	STRAIGHT,
+	CROSS,
+	CURVE,
+	T
+}
+
+enum Rotation {
+	UP,
+	LEFT,
+	DOWN,
+	RIGHT
+}
