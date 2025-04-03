b0711b13a4c35924bfdb4b96e847ead1127f82b2
SwaggiSpaghetti <uueqo@student.kit.edu>
2025-04-04 00:00:17 +0200

    BSSSSSSSSSSSSS

--- levels/bubblebee.gd
+++ levels/bubblebee.gd
@@ -3,6 +3,7 @@ extends Node2D
 var speed = 100
 var direction = Vector2.ZERO
 var screen_size
+var elapsedTime = 0
 
 func _ready():
 	direction = Vector2i(randi_range(-1,1), randi_range(-1,1))
@@ -14,6 +15,10 @@ func _ready():
 func _process(delta):
 	position -= direction * speed * delta
 	var local_pos = to_local(position)
+	elapsedTime += delta
+	if(elapsedTime > 0.1):
+		direction = Vector2(direction.x + randf_range(-0.5, 0.5), direction.y + randf_range(-0.5, 0.5)).normalized()
+		elapsedTime = 0
 
 func _change_speed_leaving_area(area: Area2D):
 	speed = -speed
