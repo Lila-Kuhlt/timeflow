7caf12ec21bef531a9690637661a8fb5f497d58a
konsumlamm <konsumlamm@gmail.com>
2025-04-04 02:14:13 +0200

    Bubblebee ist fly am been

--- levels/bubblebee.gd
+++ levels/bubblebee.gd
@@ -1,29 +1,18 @@
-extends Node2D
+extends CanvasLayer
 
-var speed = 100
-var direction = Vector2.ZERO
-var screen_size
-var elapsedTime = 0
+var speed := 0.1
+var local_pos := Vector2() # the position in our local coordinates ([0,1] x [0,1])
 @onready var path: Path2D = $Path2D
 @onready var path_follow: PathFollow2D = $Path2D/PathFollow2D
 @onready var bee_node: Node2D = $BeeNode
 
 func _ready():
-	direction = Vector2i(randi_range(-1,1), randi_range(-1,1))
-	speed = randf_range(50, 200)
-	screen_size = get_parent().to_local(get_viewport().size)
-	#var coords_vp = get_parent().to_local(get_viewport().size)
-	#position = Vector2(randf_range(scale.x, coords_vp.x - scale.x), randf_range(scale.y, coords_vp.y - scale.y))
 	update_next_path()
 
 func rand_position() -> Vector2:
-	var viewport := get_viewport_rect()
-	var vw_start: Vector2 = get_parent().to_local(viewport.position)
-	var vw_end: Vector2 = get_parent().to_local(viewport.end)
-	print(viewport)
 	return Vector2(
-		randf_range(vw_start.x, vw_end.x),
-		randf_range(vw_start.y, vw_end.y),
+		randf(),
+		randf(),
 	)
 
 func update_next_path():
@@ -40,20 +29,10 @@ func update_next_path():
 	path.curve.set_point_position(1, newpos)
 
 func _process(delta):
-	path_follow.progress += delta * 500.0
-	bee_node.global_position = path_follow.position
+	path_follow.progress += delta * speed
+	bee_node.global_position = path_follow.position * get_viewport().get_visible_rect().size
 	print('prog ', path_follow.progress_ratio)
 	print('pos ', path_follow.position)
 	if path_follow.progress_ratio >= 1.0:
 		update_next_path()
 		path_follow.progress_ratio = 0.0
-	# position -= direction * speed * delta
-	# var local_pos = to_local(position)
-	# elapsedTime += delta
-	# if(elapsedTime > 0.1):
-	# 	direction = Vector2(direction.x + randf_range(-0.5, 0.5), direction.y + randf_range(-0.5, 0.5)).normalized()
-	# 	elapsedTime = 0
-
-# func _change_speed_leaving_area(area: Area2D):
-# 	speed = -speed
-# 	direction = Vector2(direction.x + randf_range(0, 1), direction.y + randf_range(0, 1)).normalized()
